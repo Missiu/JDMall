0bbcd89ff278e3b3dee3abc26a94bfdd8b98b40b
breezeBlog <15775278810@163.com>
2022-12-12 11:48:06 +0800

    商品详情页静态页面提交

---
+++ index/shopping.js
@@ -0,0 +1,40 @@
+function dingweijs1() {
+  document.getElementById("dingwei1").style.display = "block";
+  document.getElementById("dingwei").style.backgroundColor = "white";
+}
+function dingweijs2() {
+  document.getElementById("dingwei1").style.display = "none";
+  document.getElementById("dingwei").style.backgroundColor = "#E3E4E5";
+}
+function myJdEnter() {
+  document.getElementById("myJd1").style.display = "block";
+  document.getElementById("myJd").style.backgroundColor = "white";
+}
+function myJdLeave() {
+  document.getElementById("myJd1").style.display = "none";
+  document.getElementById("myJd").style.backgroundColor = "#E3E4E5";
+}
+function qycgEnter() {
+  document.getElementById("qycg1").style.display = "block";
+  document.getElementById("qycg").style.backgroundColor = "white";
+}
+function qycgLeave() {
+  document.getElementById("qycg1").style.display = "none";
+  document.getElementById("qycg").style.backgroundColor = "#E3E4E5";
+}
+function khfwEnter() {
+  document.getElementById("khfw1").style.display = "block";
+  document.getElementById("khfw").style.backgroundColor = "white";
+}
+function khfwLeave() {
+  document.getElementById("khfw1").style.display = "none";
+  document.getElementById("khfw").style.backgroundColor = "#E3E4E5";
+}
+function wzdhEnter() {
+  document.getElementById("wzdh1").style.display = "block";
+  document.getElementById("wzdh").style.backgroundColor = "white";
+}
+function wzdhLeave() {
+  document.getElementById("wzdh1").style.display = "none";
+  document.getElementById("wzdh").style.backgroundColor = "#E3E4E5";
+}
